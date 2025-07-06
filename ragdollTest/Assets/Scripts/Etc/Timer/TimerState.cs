7bdd80d0a766d6fcb290c89999d74cd0c0c7fd26
soltshio <shanshanshien@gmail.com>
2025-07-06 18:47:41 +0900

    ゲーム終了用の判定とタイマー関係のソースコードの作成

---
+++ ragdollTest/Assets/Scripts/Etc/Timer/TimerState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//作成者:杉山
+//タイマーの状態
+
+public enum TimerState
+{
+    Off,//動いていない
+    On,//動いている
+    Pause,//一時停止中
+}
