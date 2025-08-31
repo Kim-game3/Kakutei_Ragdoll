bd9ffee69b9248ee445782a090d61b77ff830eca
soltshio <shanshanshien@gmail.com>
2025-08-31 14:42:39 +0900

    身体のパーツの荒ぶりを止めるコンポーネントの改良

--- ragdollTest/Assets/Scripts/Component/Player/StopMovePartRotate.cs
+++ ragdollTest/Assets/Scripts/Component/Player/StopMovePartRotate.cs
@@ -9,25 +9,41 @@ using UnityEngine;
 
 public class StopMovePartRotate : MonoBehaviour
 {
-    [Tooltip("身体のパーツのRigidbody")] [SerializeField] Rigidbody[] _bodyPartRbs;
-    [Tooltip("回転のしにくさ")] [SerializeField] float _rotationalInertia;
+    [CustomLabel("身体のパーツのRigidbody")] [SerializeField]
+    Rigidbody[] _bodyPartRbs;
+    [Tooltip("1だと変化なし")] [CustomLabel("回転のしにくさ")] [SerializeField]
+    float _rotationalInertia;
 
+    Vector3[] _defaultInertiaTensors;//初期の慣性テンソルの値
+
+    private void Awake()
+    {
+        _defaultInertiaTensors = new Vector3[_bodyPartRbs.Length];
+
+        //初期の慣性テンソルの値を記録
+        for(int i=0; i<_defaultInertiaTensors.Length ;i++)
+        {
+            _defaultInertiaTensors[i] = _bodyPartRbs[i].inertiaTensor;
+        }
+    }
 
     void Start()
     {
         StopRotate();
     }
 
-    //private void OnValidate()
-    //{
-    //    StopRotate();
-    //}
+    private void OnValidate()
+    {
+        StopRotate();
+    }
 
     void StopRotate()//身体のパーツを回転しにくくする
     {
-        for (int i = 0; i < _bodyPartRbs.Length; i++)
+        if (_defaultInertiaTensors == null || _bodyPartRbs==null) return;
+
+        for (int i = 0; i < _defaultInertiaTensors.Length; i++)
         {
-            _bodyPartRbs[i].inertiaTensor *= _rotationalInertia;
+            _bodyPartRbs[i].inertiaTensor = _defaultInertiaTensors[i]*_rotationalInertia;
         }
     }
 }
