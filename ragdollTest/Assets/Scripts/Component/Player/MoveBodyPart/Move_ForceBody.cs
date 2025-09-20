7aad27acb4f6db5fe7c9853cf5e2130c9edb3f66
soltshio <shanshanshien@gmail.com>
2025-09-21 00:13:02 +0900

    プレイヤーの移動速度(最大速度)の調整

--- ragdollTest/Assets/Scripts/Component/Player/MoveBodyPart/Move_ForceBody.cs
+++ ragdollTest/Assets/Scripts/Component/Player/MoveBodyPart/Move_ForceBody.cs
@@ -19,24 +19,8 @@ public class Move_ForceBody : MonoBehaviour
     [Tooltip("このオブジェクトの前方向に力が加えられる")] [CustomLabel("基準の方向")] [SerializeField]//このオブジェクトの地面に平行な+Z方向を前とする
     Transform _baseDirection;
 
-    [CustomLabel("速度制限機能")] [SerializeField]
-    JudgeIsValidSpeed _judgeIsValidSpeed;
-
     [Tooltip("かける力を足場の角度に沿わせる設定")] [SerializeField]
     FollowVectorToScaffold _followVectorToScaffold;
-
-
-    [CustomLabel("現在のプレイヤーの速度をデバッグ表示するか")]
-    [SerializeField]
-    bool _showDebug_Speed;
-
-    [CustomLabel("最大移動速度")]
-    [SerializeField]
-    float _maxSpeed;
-
-    [Tooltip("接地判定")]
-    [SerializeField]
-    JudgeIsGround _judgeIsGround;
 
     //移動時に呼ばれる(Vector3は加速方向が入る)
     public event Action OnMove;
@@ -60,14 +44,6 @@ public class Move_ForceBody : MonoBehaviour
 
     private void Move(Vector2 input)
     {
-        OnMove?.Invoke();
-
-        //if (!_judgeIsValidSpeed.IsValidSpeed())//速度が一定以上超えてたらこれ以上加速させない
-        //{
-        //    OnMove_Vec?.Invoke(Vector3.zero);
-        //    return;
-        //}
-
         Vector3 inputVec_3D = new Vector3(input.x, 0, input.y).normalized;
 
         //入力ベクトルをベースの方向(y方向は無視、z方向のみ)に向ける
@@ -78,54 +54,13 @@ public class Move_ForceBody : MonoBehaviour
 
         Vector3 forceDirection = lookForward * inputVec_3D;
 
-        Vector3 force = forceDirection * _power;
+        forceDirection = _followVectorToScaffold.Follow(forceDirection);//力をかける方向を足場の角度に沿わせる
 
-        force = _followVectorToScaffold.Follow(force);//力をかける方向を足場の角度に沿わせる
+        Vector3 force = forceDirection * _power;
 
         _body.AddForce(force,ForceMode.VelocityChange);
 
+        OnMove?.Invoke();
         OnMove_Vec?.Invoke(forceDirection);
-
-        Debug.Log("動いてるよ");
-    }
-
-    private void FixedUpdate()
-    {
-        LimitSpeed();
-    }
-
-    void LimitSpeed()
-    {
-        Vector3 velocity = _body.velocity;
-
-        if (_judgeIsGround.IsGround)
-        {
-            if (_showDebug_Speed) Debug.Log(velocity.magnitude);//速度のデバッグ表示
-
-            if(velocity.magnitude > _maxSpeed)
-            {
-                Debug.Log("制限中");
-                velocity=velocity.normalized*_maxSpeed;
-                _body.velocity=velocity;
-            }
-        }
-        else
-        {
-            velocity.y = 0;
-            if (_showDebug_Speed) Debug.Log(velocity.magnitude);//速度のデバッグ表示
-
-            if(velocity.magnitude > _maxSpeed)
-            {
-                Debug.Log("制限中");
-                velocity = velocity.normalized*_maxSpeed;
-                velocity.y = _body.velocity.y;
-                _body.velocity=velocity;
-            }
-        }
-    }
-
-    private void Awake()
-    {
-        _judgeIsValidSpeed.Init(_body);
     }
 }
