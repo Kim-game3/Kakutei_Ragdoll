4c79ff9054a2451fee869085ad468ebfc89adc80
soltshio <shanshanshien@gmail.com>
2025-09-29 22:20:18 +0900

    プレイヤーのハイスコアを管理するクラスの作成

---
+++ ragdollTest/Assets/Scripts/PureC#/PlayerData/PlayerData_HighScore/ScoreData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//作成者:杉山
+//ハイスコアデータ
+
+public class ScoreData
+{
+    float _clearTime;
+    int _deathCount;
+
+    public ScoreData(float clearTime,int deathCount)
+    {
+        _clearTime = clearTime;
+        _deathCount = deathCount;
+    }
+
+    public float ClearTime { get { return _clearTime; } }
+    public int Death {  get { return _deathCount; } }
+}
