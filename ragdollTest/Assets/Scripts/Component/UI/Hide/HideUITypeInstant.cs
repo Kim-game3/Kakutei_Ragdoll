6c11dca221f1e1d15355827ee8362552547ea5af
soltshio <shanshanshien@gmail.com>
2025-10-01 20:04:24 +0900

    リザルトシーンにCanvasGroupの配置

---
+++ ragdollTest/Assets/Scripts/Component/UI/Hide/HideUITypeInstant.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+//作成者:杉山
+//UIの非表示(すぐにパッと非表示にするタイプ)
+
+public class HideUITypeInstant : HideUITypeBase
+{
+    [CustomLabel("非表示にしたいUI群")] [SerializeField]
+    CanvasGroup _hideUIGroup;
+
+    const float _hideAlpha = 0;
+
+    public override void Hide()
+    {
+        _hideUIGroup.alpha = _hideAlpha;
+    }
+}
---
+++ ragdollTest/Assets/Scripts/Component/UI/Show/ShowUITypeInstant.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+//作成者:杉山
+//UIの表示(すぐにパッと表示するタイプ)
+
+public class ShowUITypeInstant : ShowUITypeBase
+{
+    [CustomLabel("表示したいUI群")] [SerializeField]
+    CanvasGroup _showUIGroup;
+
+    const float _showAlpha = 1;
+
+    public override void Show()
+    {
+        _showUIGroup.alpha = _showAlpha;
+    }
+}
