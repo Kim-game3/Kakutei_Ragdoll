c9f224e32780ee6a5b108601df67330c6e133eb8
soltshio <shanshanshien@gmail.com>
2025-10-01 20:54:08 +0900

    タイトル画面のコンポーネント配置

--- ragdollTest/Assets/Scripts/PureC#/PauseMenu/Option/OptionManager_UI.cs
+++ ragdollTest/Assets/Scripts/PureC#/PauseMenu/Option/OptionManager_UI.cs
@@ -15,8 +15,11 @@ public partial class OptionManager
         [SerializeField]
         EventSystem _eventSystem;
 
-        [CustomLabel("オプションメニュー")] [SerializeField]
-        GameObject _optionMenu;
+        [CustomLabel("オプションメニューを開く機能")] [SerializeField]
+        ShowUITypeBase _openOption;
+
+        [CustomLabel("オプションメニューを閉じる機能")] [SerializeField]
+        HideUITypeBase _closeOption;
 
         Button _openButton;//オプションメニューを開くボタン
         Button _closeButton;//オプションメニューを閉じるボタン
@@ -29,24 +32,24 @@ public partial class OptionManager
 
         public void Start()
         {
-            _optionMenu.SetActive(false);//ゲーム開始時にはオプションメニューは閉じておく
+            _closeOption.Hide();//ゲーム開始時にはオプションメニューは閉じておく
         }
 
         public void OnOpen()
         {
-            _optionMenu.SetActive(true);
+            _openOption.Show();
             _eventSystem.SetSelectedGameObject(_closeButton.gameObject);//選択ボタンを閉じるボタンに設定
         }
 
         public void OnClose()
         {
-            _optionMenu.SetActive(false);
+            _closeOption.Hide();
             _eventSystem?.SetSelectedGameObject(_openButton.gameObject);//選択ボタンを開くボタンに設定
         }
 
         public void OnDisable()
         {
-            if(_optionMenu!=null) _optionMenu.SetActive(false);
+            if(_closeOption != null) _closeOption.Hide();
         }
     }
     
