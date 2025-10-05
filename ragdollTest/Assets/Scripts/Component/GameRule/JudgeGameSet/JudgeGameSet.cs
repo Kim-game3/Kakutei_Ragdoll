5bd3cbece12d7cb609c2ffdf0ff5c7d2194dc7ef
soltshio <shanshanshien@gmail.com>
2025-10-05 14:57:56 +0900

    クリア演出を修正

--- ragdollTest/Assets/Scripts/Component/GameRule/JudgeGameSet/JudgeGameSet.cs
+++ ragdollTest/Assets/Scripts/Component/GameRule/JudgeGameSet/JudgeGameSet.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 //作成者:杉山
 //ゲーム終了を判断
 
-//*注意点
-//これを使う際、クリア判定用のトリガーをつけること
-
 public class JudgeGameSet : MonoBehaviour
 {
+    [SerializeField]
+    OnTriggerDetect _gameClearZone;
+
     bool _isGameSet=false;//ゲーム終了したか
 
     public bool IsGameSet {  get { return _isGameSet; } }
 
+    private void Awake()
+    {
+        _gameClearZone.OnEnter += SetGameClear;
+    }
 
-    private void OnTriggerEnter(Collider other)//ちょっと変えるかも
+    void SetGameClear(Collider other)
     {
         //クリア条件
         if (!other.CompareTag(ObjectTagNameDictionary.Player)) return;
 
         _isGameSet = true;
     }
-
 }
