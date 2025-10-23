cb0496021c436bf394e9d30ed316fb82ef92d034
soltshio <shanshanshien@gmail.com>
2025-10-23 15:57:34 +0900

    カメラバグ修正した

--- ragdollTest/Assets/Scripts/PureC#/Restart/RestartManager/InputControl_RestartManager.cs
+++ ragdollTest/Assets/Scripts/PureC#/Restart/RestartManager/InputControl_RestartManager.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,11 +13,15 @@ public partial class RestartProcess
         [SerializeField]
         PlayerInput _playerInput;
 
+        [SerializeField]
+        CinemachineInputProvider _inputProvider;
+
         public void SetControllable(bool isControllable)//操作可能状態の切り替え
         {
             string newActionMapName = isControllable ? ActionMapNameDictionary.Controllable : ActionMapNameDictionary.Restart; 
 
-            _playerInput.SwitchCurrentActionMap(newActionMapName);
+            _playerInput.SwitchCurrentActionMap(newActionMapName);//操作マップ切り替え
+            _inputProvider.enabled = isControllable;//カメラの操作切り替え
         }
     }
 }
