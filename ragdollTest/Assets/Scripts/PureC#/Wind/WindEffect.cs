dabeb91818174dda518181cbcaf25188efdd8990
soltshio <shanshanshien@gmail.com>
2025-11-02 07:20:38 +0900

    新しい風エフェクトを導入

--- ragdollTest/Assets/Scripts/PureC#/Wind/WindEffect.cs
+++ ragdollTest/Assets/Scripts/PureC#/Wind/WindEffect.cs
@@ -11,12 +11,42 @@ public class WindEffect
     [SerializeField]
     ParticleSystem _effect;
 
+    [Tooltip("自動的にサイズを調整するか")] [SerializeField]
+    bool _autoSize;
+
+    const float _effectScaleZ = 0.2f;
+
     bool _isActive=true;
 
     public void Awake()
     {
         //最初に風のエフェクトが非アクティブになってたら表示しないようにする
         _isActive=_effect.gameObject.activeSelf;
+    }
+
+    public void OnValidate(Vector3 windZoneScale)
+    {
+        //エフェクトのサイズを自動調整
+        if (!_autoSize) return;
+
+        if(_effect==null) return;
+
+        ParticleSystem[] effects = _effect.GetComponentsInChildren<ParticleSystem>();
+
+        //lifeTimeの調整
+        for(int i=0; i<effects.Length ;i++)
+        {
+            var mainModule=effects[i].main;
+            mainModule.startLifetime=windZoneScale.z/ mainModule.startSpeed.constant;
+        }
+
+        //サイズの調整
+        for(int i=0; i<effects.Length ;i++)
+        {
+            var effectShape = effects[i].shape;
+
+            effectShape.scale = new Vector3(windZoneScale.x, windZoneScale.y, _effectScaleZ);
+        }
     }
 
     public void Switchvisible(bool isActive)//可視状態を切り替え
