cf9cb68ddd7d1b838646f2f6de80f15f25314bfe
soltshio <shanshanshien@gmail.com>
2026-04-30 15:59:17 +0900

    体験版終了時のシーンを作成

---
+++ ragdollTest/Assets/Scripts/Component/GameRule/TrialFinishScene/TrialFinishSceneGameRule.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+//作成者:杉山
+//体験版終了シーンのゲームルール
+//最初の数秒フェードアウトを入れ、フェードアウトが終わった後に体験版終了のUIを表示する
+
+public class TrialFinishSceneGameRule : MonoBehaviour
+{
+    [Tooltip("シーンが始まってからUIが表示されるまでの時間")] [SerializeField]
+    float _waitDuration_s;
+
+    [SerializeField]
+    Animator _fadeAnimator;
+
+    [SerializeField]
+    Button _finishButton;
+
+
+    void Start()
+    {
+       StartCoroutine(ShowUICoroutine());
+    }
+
+    IEnumerator ShowUICoroutine()
+    {
+        //フェードイン開始
+        _fadeAnimator.SetTrigger(FadeInOutTriggerName.fadeIn);
+
+        //数秒間待つ
+        yield return new WaitForSeconds(_waitDuration_s);
+
+        //フェードイン用のスクリーンを無効化(ボタンが押せるようにする)
+        _fadeAnimator.gameObject.SetActive(false);
+        //終了ボタンを選択状態にする
+        EventSystem.current.SetSelectedGameObject(_finishButton.gameObject);
+    }
+
+
+}
---
+++ ragdollTest/Assets/Scripts/PureC#/General/AnimatorName/FadeInOutTriggerName.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//作成者:杉山
+//フェードインアウトのトリガー名をまとめたクラス
+
+public static class FadeInOutTriggerName
+{
+    public const string fadeIn = "FadeIn";
+    public const string fadeOut = "FadeOut";
+}
